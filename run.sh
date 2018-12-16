e103a0085695abc546881abdf27b05ddd988374c
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-12-16 23:52:01 +0700

    [Added]: remove access.log before create new proxy service container

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 docker rm -f proxy-service
+rm squid/log/access.log
+touch squid/log/access.log
 docker run --name proxy-service -d \
   --publish 3128:3128 \
   --volume $PWD/squid/config/squid.conf:/etc/squid/squid.conf \
@@ -7,5 +9,4 @@ docker run --name proxy-service -d \
   --volume $PWD/squid/log/access.log:/var/log/squid/access.log \
   --volume $PWD/squid/cache:/var/spool/squid \
   sameersbn/squid:3.5.27
-
 tail -f squid/log/access.log
